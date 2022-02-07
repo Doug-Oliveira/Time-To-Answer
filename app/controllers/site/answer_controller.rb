7c0685b0424c720d011ac2a426f6304915a74bba
Doug-Oliveira <douglasdc778@gmail.com>
2022-02-07 18:40:27 -0300

    add changes

---
+++ app/controllers/site/answer_controller.rb
@@ -0,0 +1,8 @@
+module Site
+  class AnswerController < SiteController
+    
+    def question
+      
+    end
+  end
+end
--- app/services/paginate.rb
+++
@@ -1,7 +0,0 @@
-class Paginate
-
-  def self.call(model, count_page)
-  debugger
-    Kaminari.paginate_array(model).page(params[:page]).per(count_page)
-  end
-end
